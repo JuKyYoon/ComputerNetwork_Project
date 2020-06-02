68e9ea0d2ca3bf95ce37b150a9e3d25111c70ca3
JuKyYoon <yoongh97@gmail.com>
2020-06-02 13:45:55 +0900

    handling other file extension
    not complete

--- server.c
+++ server.c
@@ -5,13 +5,18 @@
 #include <unistd.h> 
 #include <stdlib.h> // exit 사용
 #include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define MSG_SIZE 4096
 
 void error_print(char *error_msg);
 void request_print(char *response_msg, char *request_msg);
-void request_file(char *response_msg, char *request_msg, char *file_extension);
+void request_file(char *response_msg, char *request_msg, char *filename, char *file_extension);
 void request_handle(char *response_msg, char *request_msg);
+void request_html_file(char *response_msg, char *request_msg, char *filename);
+void request_mp3_file(char *response_msg, char *request_msg, char *filename);
+void request_image_file(char *response_msg, char *request_msg, char *filename);
 char* get_content_type(char *filename, char *file_extension);
 
 int main(int argc, char *argv[]){
@@ -63,25 +68,31 @@ int main(int argc, char *argv[]){
 
     while(1){
         memset(response_msg, 0 ,MSG_SIZE);
-        memset(request_msg, 0 ,MSG_SIZE);       
+        memset(request_msg, 0 ,MSG_SIZE*2);   
         if( (socket_fd = accept( server_socket_fd, (struct sockaddr *)&client_addr, &req_client)) < 0){
             error_print("Fail to accpet");
         }
 
+        
         printf("connection is successful : address: %s, port = % d\n", 
             inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
         
- 
+       
         if( read(socket_fd, request_msg, MSG_SIZE) < 0 ){
             error_print("Fail to read");
         }
+        printf("accept the request\n");
+        // printf("%s\n",request_msg);
         
         request_handle(response_msg, request_msg);
+        printf("@@@@@@@@@@@@@@@\n%s\n@@@@@@@@@@@@@@@@\n",response_msg);
         if ( write(socket_fd, response_msg, strlen(response_msg))  < 0 ) { 
             error_print("Fail to writing to socket.");
         }
-
+           
         close(socket_fd);
+        printf("----------------\n");
+        
     }
 
     free(response_msg);
@@ -98,10 +109,11 @@ void request_handle(char *response_msg, char *request_msg){
     strcpy(request_msg_copy, request_msg);
     // printf("\n%s\n", request_msg_copy);
     char method[10];
-    char filename[30]; // '/' 로 잘라서 request 분석해보장
+    char *filename; // '/' 로 잘라서 request 분석해보장
     char content_type[50];
     char *file_extesion;
     file_extesion = malloc(sizeof(char)*30);
+    filename = malloc(sizeof(char)*100);
     strcpy(method, strtok(request_msg_copy, " ")); //method 'GET'
     strcpy(filename, strtok(NULL," ")); // file name  ex) '/index.html', '/'
 
@@ -110,19 +122,16 @@ void request_handle(char *response_msg, char *request_msg){
     }
     else{ // 입력받은 파일이 있을 경우
         strcpy(content_type, get_content_type(filename,file_extesion)); // 파일의 확장자 명을 가져온다.
-        if( !strcmp(content_type, "nomake")){ // 확장자 얻는 과정에서 이상이 있다.
+        if( !strcmp(content_type, "nomake") || access(filename+1,F_OK) < 0 ){ // 확장자 얻는 과정에서 이상이 있다. 혹은 해당 파일이 없다.
             // char error_msg[20] = "HTTP 404 NOT FOUND";
             // int error_len = (int)strlen(error_msg) + 7;
             sprintf(response_msg, "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-length: 27\r\n\r\n<h1>HTTP 404 NOT FOUND</h1>"); // 404출력
         }
         else{ // 아무런 이상이 없다.
-            request_file(response_msg, request_msg, file_extesion); // 파일 확장자에 따라 파일을 처리해 준다.
+            printf("request file function %s \n", filename);
+            request_file(response_msg, request_msg, filename+1, file_extesion); // 파일 확장자에 따라 파일을 처리해 준다.
         }
     }
-    
-
-    
-    
 }
 
 
@@ -139,27 +148,41 @@ void request_print(char *response_msg, char *request_msg){
     sprintf(response_msg, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-length: %d\r\n\r\n<h>%s</h>",content_length,req);
 }
 
-void request_file(char *response_msg, char *request_msg, char *file_extension){
-    //html일 경우에만 파일 읽어서 가져와야 한다.
-    FILE* openfile;
-    char method[10];
-    char filename[30]; // '/' 로 잘라서 request 분석해보장
-    if( strcmp(filename, "favicon.ico" ) == 0) { return ;} // segmantation 오류 임시 방편
-    char proto[] = "HTTP/1.1 200 OK\r\n";
-    char content_type[] = "Content-Type: text/html\r\n"; // filename을 strtok해서 얻어야 될듯 일단 html로 고정시키자.
-    // printf("%s\n",filename);
+void request_file(char *response_msg, char *request_msg, char *filename, char *file_extension){
+    //맨 처음엔
+    if(!strcmp(file_extension, "html")){
+        request_html_file(response_msg, request_msg, filename);
+    }
+    else if(!strcmp(file_extension, "mp3")){
+        request_mp3_file(response_msg, request_msg, filename);
+    }
+    else if(!strcmp(file_extension, "jpeg") || !strcmp(file_extension, "jpg")){
+        request_image_file(response_msg, request_msg, filename);
+    }
+}
+
+void request_html_file(char *response_msg, char *request_msg, char *filename){
+    FILE* openfile; // html 파일
     openfile = fopen(filename, "r"); // read로 열자.
-    char *req = malloc(sizeof(char)*MSG_SIZE);
-    int idx = 0;
-    int c;
+    char *req = malloc(sizeof(char)*MSG_SIZE); // 메모리 아낄러면 response에 계속 strcat 하는게 제일 좋을 듯.
+    int idx = 0, c;
     while( (c = fgetc(openfile)) != EOF ){
         req[idx++] = c;
     }
     fclose(openfile);
+    sprintf(response_msg, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-length: %ld\r\n\r\n%s",strlen(req), req);
+}
 
-    int content_length = strlen(req);
-    sprintf(response_msg, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-length: %d\r\n\r\n%s",content_length, req);
+void request_mp3_file(char *response_msg, char *request_msg, char *filename){
+    char *req = malloc(sizeof(char)*MSG_SIZE); // 메모리 아낄러면 response에 계속 strcat 하는게 제일 좋을 듯.
+    sprintf(req, "<!DOCTYPE html>\n<body>\n<figure>\n\t<audio controls>\n\t<source=\"%s\">Your browser does not support the<code>audio</code> element.\n\t</audio>\n</figure>\n</body>\n</html>",filename);
+    sprintf(response_msg, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-length: %ld\r\n\r\n%s",strlen(req), req);
+}
 
+void request_image_file(char *response_msg, char *request_msg, char *filename){
+    char *req = malloc(sizeof(char)*MSG_SIZE); // 메모리 아낄러면 response에 계속 strcat 하는게 제일 좋을 듯.
+    sprintf(req, "<!DOCTYPE html>\n<body>\n<h1>image</h1>\n<img src=\"%s\"/>\n</body>\n</html>",filename);
+    sprintf(response_msg, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-length: %ld\r\n\r\n%s",strlen(req), req);
 }
 
 char* get_content_type(char *filename, char *file_extension){
@@ -189,6 +212,12 @@ char* get_content_type(char *filename, char *file_extension){
     }
     if(strcmp(file_type+1, "mp3") == 0){
         return "audio/mpeg3";
+    }
+    if(!strcmp(file_type+1, "gif")){
+        return "image/gif";
+    }
+    if(!strcmp(file_type+1, "jpg") || !strcmp(file_type+1, "jpeg")){
+        return "image/jpeg";
     }
     return "nomake"; // 구현이 안 된것.
 }
